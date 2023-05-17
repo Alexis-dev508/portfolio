b205d0be3208db6c70a2f4f597a638df0a024bb2
Alexis-dev508 <alexisjesus773@gmail.com>
2023-05-17 13:57:00 -0700

    map fixed

--- public_html/assets/js/johndoe.js
+++ public_html/assets/js/johndoe.js
@@ -59,8 +59,8 @@ $(window).on("load", function() {
 // google maps
 function initMap() {
 // Styles a map in night mode.
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 40.674, lng: -73.945},
+    var map = new google.maps.Map(document.getElementById('map'), { 
+        center: {lat: 25.214550, lng: -106.572448},
         zoom: 12,
         scrollwheel:  false,
         navigationControl: false,
